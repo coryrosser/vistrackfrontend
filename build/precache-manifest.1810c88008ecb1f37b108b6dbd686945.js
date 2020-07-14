b155004030b073ef6a33d957d6582308f509f16d
Cortlyn Rosser <coryrosser@gmail.com>
2020-07-14 14:27:40 -0400

    add build to scource control

---
+++ build/precache-manifest.1810c88008ecb1f37b108b6dbd686945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34a4cbad293832874487078fbfe2ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57004723f6a11f7875c8",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cf99e285fdaa78739abc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "57004723f6a11f7875c8",
+    "url": "/static/js/2.96d1cc95.chunk.js"
+  },
+  {
+    "revision": "9c3091dd6db592dedb0cf061649d04a2",
+    "url": "/static/js/2.96d1cc95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf99e285fdaa78739abc",
+    "url": "/static/js/main.73b4517e.chunk.js"
+  },
+  {
+    "revision": "33e4c881da48772cefc4",
+    "url": "/static/js/runtime-main.20499e23.js"
+  }
+]);
